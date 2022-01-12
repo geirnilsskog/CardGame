89c591e4fd14eae9492d5047b08c74145c4e0c4c
geirnilsskog <geir@nilsskog.net>
2022-01-12 13:30:13 +0100

    Initial commit

---
+++ CardGame/Classes/Common.cs
@@ -0,0 +1,113 @@
+namespace CardGame.Common
+{
+    public class Card
+    {
+        public CardType CardType;
+        public int Value;
+
+        public Card(CardType type, int value)
+        {
+            CardType = type;
+            Value = value;
+        }
+    }
+
+    public enum CardType
+    {
+        Invalid = 0,
+        Diamonds = 1,
+        Hearts = 2,
+        Spades = 3,
+        Clubs = 4
+    }
+
+    public enum PlayerStrategy
+    {
+        Invalid = 0,
+        MatchPrizeCard = 1,
+        RandomCard = 2,
+    }
+
+    public class PlayerCard
+    {
+        public Player Player;
+        public Card Card;
+
+        public PlayerCard(Player player, Card card)
+        {
+            Player = player;
+            Card = card;
+        }
+    }
+
+    public class Player
+    {
+        public string Name = "";
+        public PlayerStrategy Strategy = PlayerStrategy.Invalid;
+        public CardType CardType = CardType.Invalid;
+        public List<Card> CardsAvailable = new List<Card>();
+        public List<Card> CardsUsed = new List<Card>();
+        public List<Card> CardsWon = new List<Card>();
+
+        public int Score
+        {
+            get
+            {
+                int score = 0;
+                foreach(Card card in CardsWon)
+                {
+                    score += card.Value;
+                }
+                return score;
+            }
+        }
+
+        public Card GetBestCard(int prizeCardValue)
+        {
+            if(this.Strategy == PlayerStrategy.MatchPrizeCard)
+            {
+                // Find a card that matches the prize card value
+                List<Card> cards = CardsAvailable.Where(x => x.Value == prizeCardValue).ToList();
+
+                if(cards[0] == null)
+                {
+                    throw new Exception("Unexpected error: A card with a matching value could not be found.");
+                }
+                else
+                {
+                    CardsAvailable.Remove(cards[0]);
+                    CardsUsed.Add(cards[0]);
+                    return cards[0];
+                }
+            }
+            else if(this.Strategy == PlayerStrategy.RandomCard)
+            {
+                // Shuffle the available cards
+                List<Card> cards = CardsAvailable.OrderBy (x => new Random().Next()).ToList();
+
+                if(cards[0] == null)
+                {
+                    throw new Exception("Unexpected error: A card with a matching value could not be found.");
+                }
+                else
+                {
+                    CardsAvailable.Remove(cards[0]);
+                    CardsUsed.Add(cards[0]);
+                    return cards[0];
+                }
+            }
+            else
+            {
+                throw new Exception("Unexpected error: The player strategy " + this.Strategy.ToString() + " is not supported.");
+            }
+        }
+
+        public Player(string name, List<Card> cardsAvailable, PlayerStrategy strategy, CardType cardType)
+        {
+            Name = name;
+            CardType = cardType;
+            Strategy = strategy;
+            CardsAvailable = cardsAvailable.Where(x => x.CardType == cardType).ToList();
+        }
+    }
+}
---
+++ CardGame/Program.cs
@@ -0,0 +1,193 @@
+﻿using CardGame.Common;
+
+namespace CardGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PlayCardGame();
+        }
+
+        private static void PlayCardGame()
+        {
+            // Set up a card deck with cards minimum value 2 and maximum value 9
+            Console.WriteLine("Setting up card deck...");           
+            List<Card> cards = SetUpCardDeck(2, 9);          
+            Console.WriteLine("Card deck created, the deck contains a total of " + cards.Count() + " cards");
+
+            List<Player> players = new List<Player>();
+
+            // Set up 2 players with different names, strategies and card types
+            players.Add(new Player("Player 1", cards, PlayerStrategy.MatchPrizeCard, CardType.Clubs));
+            players.Add(new Player("Player 2", cards, PlayerStrategy.RandomCard, CardType.Diamonds));
+
+            // Set up the prize cards and shuffle these cards
+            List<Card> prizeCards = ShuffleCards(cards.Where(x => x.CardType == CardType.Spades).ToList());
+
+            int roundNumber = 1;
+
+            while(prizeCards.Count > 0)
+            {
+                // Pick the next prize card and remove it so that it will only be used this one time
+                Card prizeCard  = prizeCards[0];
+                prizeCards.Remove(prizeCard);
+
+                Console.WriteLine("");
+                Console.WriteLine("------------------------------------------------------------------------------------------");
+                Console.WriteLine("ROUND# " + roundNumber.ToString());
+                Console.WriteLine("");
+                Console.WriteLine("Prize card drawn: " + prizeCard.CardType.ToString() + " " + prizeCard.Value.ToString());
+
+                List<Player> winners = PickWinners(players, prizeCard);
+
+                
+                if(winners.Count == 1)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("ROUND WINNER: " + winners[0].Name + " (+" + prizeCard.Value + ")");    
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    string output = "NO ROUND WINNER, EVEN SCORE: ";
+
+                    for(int i=0;i<winners.Count;i++)
+                    {
+                        if(i > 0) output += " and ";
+                        output += winners[0].Name;
+                    }
+
+                    output += " (+" + prizeCard.Value + ")";
+                    Console.WriteLine(output);
+                }
+
+                foreach(Player winner in winners)
+                {
+                    winner.CardsWon.Add(prizeCard);
+                }
+
+                WriteGameStatusToConsole(players, prizeCards);
+                roundNumber++;
+            }
+
+            WriteGameFinalScoreToConsole(players);
+        }
+
+        private static List<Player> PickWinners(List<Player> players, Card prizeCard)
+        {
+            List<Player> winners = new List<Player>();
+
+            List<PlayerCard> playerCards = new List<PlayerCard>();
+
+            foreach(Player player in players)
+            {
+                Card card = player.GetBestCard(prizeCard.Value);
+                PlayerCard playerCard = new (player, card);
+                playerCards.Add(playerCard);
+
+                Console.WriteLine(playerCard.Player.Name + " used " + playerCard.Card.CardType.ToString() + " " + playerCard.Card.Value.ToString());
+            }
+
+            // Find the highest card value
+            int highestValue = 0;
+
+            foreach(PlayerCard playerCard in playerCards)
+            {
+                if(playerCard.Card.Value > highestValue) highestValue = playerCard.Card.Value;
+            }
+
+            // Players that used a card with highest value will be the winners
+            foreach(PlayerCard playerCard in playerCards)
+            {
+                if(playerCard.Card.Value == highestValue) winners.Add(playerCard.Player);                
+            }
+
+            return winners;
+        }
+
+        private static void WriteGameStatusToConsole(List<Player> players, List<Card> prizeCards)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("SCOREBOARD (" + prizeCards.Count + " card(s) remaining)");
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine(String.Format("| {0,-15} | {1,-15} | {2,-15} |", "Player", "Card Type", "Score"));
+            Console.WriteLine("-------------------------------------------------------");
+           
+            int highestScore = 0;
+
+            foreach(Player player in players)
+            {
+                if(player.Score > highestScore) highestScore = player.Score;
+
+                Console.WriteLine(String.Format("| {0,-15} | {1,-15} | {2,-15} |", player.Name, player.CardType.ToString(), player.Score.ToString()));
+            }
+
+            Console.WriteLine("-------------------------------------------------------");
+        }
+
+        private static void WriteGameFinalScoreToConsole(List<Player> players)
+        {
+            List<Player> winners = new List<Player>();
+
+            Console.WriteLine("");
+
+            // Find the highest card value
+            int highestScore = 0;
+
+            foreach(Player player in players)
+            {
+                if(player.Score > highestScore) highestScore = player.Score;
+            }
+
+            foreach(Player player in players)
+            {
+                if(player.Score == highestScore) winners.Add(player);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("------------------------------------------------------------------------------------------");
+
+            if(winners.Count == 1)
+            {                   
+                Console.WriteLine("GAME WINNER: " + winners[0].Name);    
+            }
+            else
+            {
+                string output = "NO WINNER - EVEN SCORES: ";
+
+                for(int i=0;i<winners.Count;i++)
+                {
+                    if(i > 0) output += " and ";
+                    output += winners[i].Name;
+                }
+
+                Console.WriteLine(output);   
+            }
+        }
+
+        private static List<Card> SetUpCardDeck(int minValue, int maxValue)
+        {
+            List<Card> cards = new List<Card>();
+
+            for(int i=1;i<5;i++) // CardType 1 to 4
+            {
+                for(int j=minValue;j<maxValue+1;j++) // We're only going to use cards with values 2 to 9
+                {
+                    CardType type = (CardType)Enum.Parse(typeof(CardType), i.ToString());
+                    Card card = new Card(type, j);
+                    cards.Add(card);
+                }
+            }
+
+            return cards;
+        }
+
+        private static List<Card> ShuffleCards(List<Card> cards)
+        {
+            Random random = new Random();
+            List<Card> shuffledCards = cards.OrderBy (x => random.Next()).ToList();
+            return shuffledCards;
+        }
+    }    
+}
